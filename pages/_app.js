8a7479e72ace400fc600e93d55710a0608647404
vianarafael <vianarafaelds@gmail.com>
2021-07-29 07:02:35 +0900

    added a footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import theme from "../src/theme/";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
+import GlobalStyle from "../src/theme/GlobalStyle";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <title>Rafagram</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
