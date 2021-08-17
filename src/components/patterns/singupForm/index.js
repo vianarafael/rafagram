0109dfe867cb47338352da599731c98cfb001784
vianarafael <vianarafaelds@gmail.com>
2021-08-17 20:38:21 +0900

    set the form w/ simple validation

--- src/components/patterns/singupForm/index.js
+++ src/components/patterns/singupForm/index.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function FormContent() {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    user: "",
+  });
+
+  function handleChange(e) {
+    const fieldName = e.target.getAttribute("name");
+
+    setUserInfo({ ...userInfo, [fieldName]: e.target.value });
+  }
+
+  const isFormInvalid =
+    userInfo.email.length === 0 || userInfo.user.length === 0;
+
   return (
     <form>
       <div>
-        <input placeholder="Email" />
-        <input placeholder="User" />
-        <button type="submit">Sign Up</button>
+        <input
+          placeholder="Email"
+          name="email"
+          value={userInfo.email}
+          onChange={handleChange}
+        />
       </div>
+      <div>
+        <input
+          placeholder="User"
+          name="user"
+          value={userInfo.name}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button
+        type="submit"
+        disabled={isFormInvalid}
+        onClick={(e) => {
+          e.preventDefault();
+          console.log(userInfo);
+        }}
+      >
+        Sign Up
+      </button>
     </form>
   );
 }
