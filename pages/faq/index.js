81a63449a605ffb150bc4f06375931a316c700de
Rafael Viana <rviana@hugeinc.com>
2021-08-28 14:27:49 +0900

    style: erased comments

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,9 +1,8 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import FAQScreen from "../../src/components/screens/FAQScreen";
 
 export default function FAQPage({ faqCategories }) {
-  // const [faqCategories, setFaqCategories] = useState([]);
-
   return <FAQScreen faqCategories={faqCategories} />;
 }
 
